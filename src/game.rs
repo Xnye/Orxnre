b26d375984b33e397c1808c36755a09d27cd6d6a
Xnye <53325931+Xnye@users.noreply.github.com>
2024-01-04 21:54:20 +0800

    v1.0-beta.6

--- src/game.rs
+++ src/game.rs
@@ -1,4 +1,4 @@
-use std::process::exit;
+use std::{process::exit};
 use colored::*;
 use console::Key;
 use noise::{NoiseFn, Perlin};
@@ -6,30 +6,33 @@ use rand::{Rng, thread_rng};
 
 use Key::Char;
 
-use crate::{data, Buffer, cls, cls_pro, read};
-use crate::data::block_name;
+use crate::{data, battle, Buffer, cls, cls_pro, read};
+use data::block_name;
+use crate::battle::Enemy;
 
 // 玩家数据
-struct Player {
+pub struct Player {
     position: (u8, u8),
     money: i32,
     max_hp: i32,
     hp: i32,
+    atk: i32,
 }
 
 impl Player {
-    fn new() -> Self { Player { position: (0, 0), money: 0, max_hp: 500, hp: 500 } }
+    fn new() -> Self { Player { position: (0, 0), money: 0, max_hp: 500, hp: 500, atk: 20 } }
 }
 
 // 地图数据
 struct Map {
     id: Vec<Vec<i8>>,
     gift: Vec<Vec<i32>>,
+    sp: Vec<Vec<Enemy>>,
 }
 
 impl Map {
-    fn create(x: usize, y: usize) -> Self {
-        Map { id: vec![vec![-1; x]; y], gift: vec![vec![0; x]; y] }
+    fn new(x: usize, y: usize) -> Self {
+        Map { id: vec![vec![-1; x]; y], gift: vec![vec![0; x]; y], sp: vec![vec![Enemy::new(); x]; y] }
     }
 
     fn measure(&self) -> (usize, usize) {
@@ -112,16 +115,17 @@ impl Map {
 }
 
 pub fn main() {
-    let mut b = Buffer::new();
-    let mut h = Buffer::new();
+    let mut b = Buffer::new(); // 主要缓冲区 打印所有内容
+    let mut h = Buffer::new(); // 打印提示信息用
 
-    let mut map = Map::create(16, 16);
-    let (x_len, y_len) = map.measure();
+    let mut map = Map::new(16, 16); // 初始化地图信息
+    let (x_len, y_len) = map.measure(); // 并获取其长度
 
-    let mut player = Player::new();
+    let mut player = Player::new(); // 初始化玩家信息
 
-    let seed: u32 = thread_rng().gen();
+    let seed: u32 = thread_rng().gen(); // 种子
 
+    // 生成地形以及宝藏
     map.map_terrain(2, seed - 1, 200.0);
     map.map_terrain(1, seed - 2, 150.0);
     map.map_set(0, 3, 11);
@@ -132,17 +136,16 @@ pub fn main() {
     loop {
         cls();
 
-        b.wl(format!("{} | {}", data::TITLE(), data::VERSION));
-        // 标题
-        b.wl(format!("${} | {}/{}", player.money, player.hp, player.max_hp));
-        // 玩家信息
+        b.wl(format!("{} | {}", data::TITLE(), data::VERSION)); // 标题
+        // Orxnre | v1.0-beta.3
 
-        b.wl(map.print(player.position));
-        b.wl(h.read());
-        // 地图
+        b.wl(format!("${} | {}/{}", player.money, player.hp, player.max_hp)); // 玩家信息
+        // $514 | 495/500
 
-        b.print();
-        h = Buffer::new();
+        b.wl(map.print(player.position)); // 写入地图
+        b.wl(h.read()); // 写入提示消息
+        b.print(); // 打印到屏幕
+        h = Buffer::new(); // 清空消息
 
         let (mut next_y, mut next_x) = player.position;
 
@@ -165,18 +168,22 @@ pub fn main() {
                     h.w(if gift != 0 {
                         map.gift[player.position.0 as usize][player.position.1 as usize] = 0;
                         player.money += gift;
-                        format!("E → 找到了宝藏 $+{}{}", gift, data::SPACES) }
-                    else {
-                        format!("E → 空空如也{}", data::SPACES) });
+                        format!("E → 找到了宝藏 $+{}{}", gift, data::SPACES)
+                    } else {
+                        format!("E → 空空如也{}", data::SPACES)
+                    });
                 }
 
                 // Others 处理默认情况
                 _ => {}
             };
         }
-        if next_y < y_len as u8 && next_x < x_len as u8 {
-            player.position = (next_y, next_x);
-        }
 
+        // 判断下一步是否合法
+        if next_y < y_len as u8 && next_x < x_len as u8 {
+            if map.id[next_y as usize][next_x as usize] != -1 {
+                player.position = (next_y, next_x);
+            }
+        }
     }
 }
